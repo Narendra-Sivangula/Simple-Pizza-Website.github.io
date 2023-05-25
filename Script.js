6f1511efc2f661384e849e366e5a17d6f198af4c
Narendra Kumar Sivangula <narendrakumarsivangula@gmail.com>
2023-05-25 07:35:18 +0530

    Script.js

--- Script.js
+++ Script.js
@@ -4,10 +4,11 @@ function calculateInterest() {
     var startDate = new Date(document.getElementById('startDate').value);
     var endDate = new Date(document.getElementById('endDate').value);
 
-    var timeInMilliseconds = endDate.getTime() - startDate.getTime();
-    var timeInYears = timeInMilliseconds / (1000 * 3600 * 24 * 365);
+    var months = (endDate.getFullYear() - startDate.getFullYear()) * 12;
+    months -= startDate.getMonth() + 1;
+    months += endDate.getMonth();
 
-    var interest = (principal * rate * timeInYears) / 100;
+    var interest = (principal * rate * months) / 100;
     var totalAmount = principal + interest;
 
     document.getElementById('result').innerHTML = 'Simple Interest: ' + interest.toFixed(2) + '<br>Total Amount: ' + totalAmount.toFixed(2);
